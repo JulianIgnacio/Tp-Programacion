925fcc9b84a351231779f84264ef1619991f1c7b
JulianIgnacio <montenegrojulianig@gmail.com>
2023-06-02 14:57:44 -0300

    finalizando Ejercicio 1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -8,8 +8,28 @@ namespace Ejercicio1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] Sueldos = new int[5];
+            Cargar(Sueldos);
+            Mostrar(Sueldos);
+            Console.ReadKey();
+        }
+        static void Cargar(int[] Sueldos)
+        {
+            Console.WriteLine("Ingrese los sueldos");
+            Sueldos[0] = Convert.ToInt32(Console.ReadLine());
+            Sueldos[1] = Convert.ToInt32(Console.ReadLine());
+            Sueldos[2] = Convert.ToInt32(Console.ReadLine());
+            Sueldos[3] = Convert.ToInt32(Console.ReadLine());
+            Sueldos[4] = Convert.ToInt32(Console.ReadLine());
+        }
+        static void Mostrar(int[] Sueldos)
+        {
+            for(int i = 0; i < Sueldos.Length; i++)
+            {
+                Console.WriteLine("El valor de la posicion {0} es: {1}",i+1,Sueldos[i]);
+            }
         }
     }
 }
